89c4d14c0bc0a07c2dd73d921629e74b5bf53940
techflashYT <officialTechflashYT@gmail.com>
2025-12-07 04:20:58 -0700

    allocator: Wii U support

--- src/allocator.c
+++ src/allocator.c
@@ -228,6 +228,19 @@ void M_Init(void) {
 		memcpy(pools[1].magic, POOL_HDR_MAGIC, POOL_HDR_MAGIC_SIZE);
 	}
 	case CONSOLE_TYPE_WII_U: {
+		/* MEM1 below our binary */
+		pools[0].top = &__reloc_dest_start;
+		pools[0].bottom = (void *)0x80004000;
+		pools[0].cur_bottom = pools[0].top;
+		pools[0].name = "MEM1";
+		memcpy(pools[0].magic, POOL_HDR_MAGIC, POOL_HDR_MAGIC_SIZE);
+
+		/* MEM2 */
+		pools[1].top = (void *)(MEM2_CACHED_BASE + MEM2_SIZE_WIIU);
+		pools[1].bottom = (void *)MEM2_CACHED_BASE;
+		pools[1].cur_bottom = pools[1].top;
+		pools[1].name = "MEM2";
+		memcpy(pools[1].magic, POOL_HDR_MAGIC, POOL_HDR_MAGIC_SIZE);
 	}
 	}
 	printf("Memory pool 0: \"%s\", 0x%08x down to 0x%08x\r\n", pools[0].name, pools[0].top, pools[0].bottom);
