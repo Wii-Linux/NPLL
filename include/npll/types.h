ad53a66a280e523eade0b848f268bb993614d1f3
techflashYT <officialTechflashYT@gmail.com>
2026-01-02 21:19:22 -0700

    types: bools

--- include/npll/types.h
+++ include/npll/types.h
@@ -30,4 +30,14 @@ typedef volatile i64        vi64;
 
 #define NULL ((void *)0)
 
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ > 201710L
+/* bool, true, and false are already defined */
+#else /* __STDC_VERSION__ && __STDC_VERSION__ > 201710L */
+typedef int bool;
+#define true ((bool)1)
+#define false ((bool)0)
+#endif /* !(__STDC_VERSION__ && __STDC_VERSION__ > 201710L) */
+
+typedef u32 size_t;
+
 #endif /* _TYPES_H */
