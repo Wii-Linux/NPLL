c50bee1a7d544e367523b5cf606dd59e46686ed3
techflashYT <officialTechflashYT@gmail.com>
2026-01-31 00:55:11 -0700

    stdio,printf: fctprintf+fctvprintf

--- include/stdio.h
+++ include/stdio.h
@@ -18,6 +18,8 @@ extern int sprintf(char* buffer, const char* format, ...);
 extern int snprintf(char* buffer, size_t count, const char* format, ...);
 extern int vsnprintf(char* buffer, size_t count, const char* format, va_list va);
 extern int vprintf(const char* format, va_list va);
+extern int fctvprintf(void (*out)(char character, void* arg), void* arg, const char* format, va_list va);
+extern int fctprintf(void (*out)(char character, void* arg), void* arg, const char* format, ...);
 
 extern int puts(const char *s);
 extern int putchar(int c);
