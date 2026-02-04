f90388494a639a9389a85ad6a8ccb7e819c1c6bc
techflashYT <officialTechflashYT@gmail.com>
2026-02-03 17:15:46 -0700

    panic: disable IRQs

--- src/panic.c
+++ src/panic.c
@@ -7,9 +7,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <npll/console.h>
+#include <npll/irq.h>
 #include <npll/video.h>
 
 void __attribute__((noreturn)) panic(const char *str) {
+	IRQ_Disable();
 	printf("FATAL: PANIC: %s\r\n", str);
 	if (V_ActiveDriver)
 		V_Flush();
