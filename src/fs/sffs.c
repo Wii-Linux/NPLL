d4930b4a6833695c3a85de6723817ee2a351b251
techflashYT <officialTechflashYT@gmail.com>
2026-05-02 21:26:04 -0700

    sffs: read support

--- src/fs/sffs.c
+++ src/fs/sffs.c
@@ -10,6 +10,8 @@
 #include <npll/block.h>
 #include <npll/endian.h>
 #include <npll/fs.h>
+#include <npll/hollywood/aes.h>
+#include <npll/hollywood/otp.h>
 #include <npll/log.h>
 #include <npll/partition.h>
 #include <npll/utils.h>
@@ -20,6 +22,9 @@
 #define SFFS_SUPERBLOCK_OFFSET 0x3f600u
 #define SFFS_SUPERBLOCK_PAGES (sizeof(struct sffs_superblock) / NAND_PAGE_SIZE)
 #define NAND_PAGE_SIZE 2048
+#define SFFS_PAGES_PER_CLUSTER 8u
+#define SFFS_CLUSTER_SIZE (NAND_PAGE_SIZE * SFFS_PAGES_PER_CLUSTER)
+#define SFFS_FIRST_DATA_CLUSTER 0x40u
 
 /* lengths of the FAT and FST arrays embedded in the superblock */
 #define SFFS_FAT_LEN 0x8000u
@@ -60,6 +65,7 @@ struct sffs_superblock {
 	u8 pad[20];
 } __attribute__((packed));
 
+static struct partition *mountedPart = NULL;
 static struct sffs_superblock sb ALIGN(32);
 struct sffsFdInfo {
 	bool open;
@@ -109,6 +115,19 @@ static int findNewestSuperblock(struct partition *part, uint *off) {
 
 	*off = bestOffset;
 	return 0;
+}
+
+static u16 sffsWalkChain(u16 first, u32 n) {
+	u16 cluster = first;
+
+	while (n--) {
+		if (cluster >= SFFS_FAT_LEN)
+			return CLUSTER_END;
+		cluster = sb.fat[cluster];
+	}
+	if (cluster >= SFFS_FAT_LEN)
+		return CLUSTER_END;
+	return cluster;
 }
 
 static int sffsLookupEntry(const char *path, struct sffs_fst_entry **out) {
@@ -194,6 +213,7 @@ static int sffsMount(struct filesystem *fs, struct partition *part) {
 	}
 
 	memset(files, 0, sizeof(files));
+	mountedPart = part;
 
 	return 0;
 }
@@ -201,6 +221,7 @@ static int sffsMount(struct filesystem *fs, struct partition *part) {
 static void sffsUnmount(struct filesystem *fs) {
 	(void)fs;
 	memset(files, 0, sizeof(files));
+	mountedPart = NULL;
 }
 
 static int sffsOpen(struct filesystem *fs, const char *path) {
@@ -232,14 +253,90 @@ static void sffsClose(struct filesystem *fs, int fd) {
 }
 
 
+static ssize_t sffsRead(struct filesystem *fs, int fd, void *dest, size_t len) {
+	u32 clusterIdx, clusterOff, pageNum, pageOff, toCopy, page;
+	u16 cluster;
+	size_t total;
+	u8 encBuf[2048] ALIGN(32);
+	u8 decBuf[2048] ALIGN(32);
+	struct sffsFdInfo *file;
+	u32 key[4];
+	u32 iv[4] = {0, 0, 0, 0};
+	int ret;
+
+	(void)fs;
+
+	VALIDATE_FD(-EBADF);
+
+	total = 0;
+
+	file = &files[fd];
+
+	/* clamp at EOF */
+	if (file->pos >= file->size)
+		return 0;
+	if (len > (size_t)(file->size - file->pos))
+		len = file->size - file->pos;
+
+	memcpy(key, H_OTPContents.wii.nandKey, sizeof(key));
+	memset(iv, 0, sizeof(iv));
+
+	while (total < len) {
+		clusterIdx = file->pos / SFFS_CLUSTER_SIZE;
+		clusterOff = file->pos % SFFS_CLUSTER_SIZE;
+		pageNum = clusterOff / NAND_PAGE_SIZE;
+		pageOff = clusterOff % NAND_PAGE_SIZE;
+		toCopy = NAND_PAGE_SIZE - pageOff;
+
+		if (toCopy > len - total)
+			toCopy = (u32)(len - total);
+
+		/* if this was a super common operation this could be cached, but eh */
+		cluster = sffsWalkChain(file->firstClust, clusterIdx);
+		if (cluster == CLUSTER_END)
+			break;
+		if (cluster < SFFS_FIRST_DATA_CLUSTER)
+			return total > 0 ? (ssize_t)total : -EIO;
+
+		page = ((u32)cluster - SFFS_FIRST_DATA_CLUSTER) * SFFS_PAGES_PER_CLUSTER + pageNum;
+		if (nandReadPage(mountedPart, encBuf, NAND_PAGE_SIZE, page) != NAND_PAGE_SIZE)
+			return total > 0 ? (ssize_t)total : -EIO;
+
+		if (!pageNum)
+			ret = H_AESDecrypt(encBuf, decBuf, iv, key, NAND_PAGE_SIZE);
+		else
+			ret = H_AESDecrypt(encBuf, decBuf, NULL, NULL, NAND_PAGE_SIZE);
+
+		ret = H_AESDecrypt(encBuf, decBuf, iv, key, NAND_PAGE_SIZE);
+		if (ret)
+			return total > 0 ? (ssize_t)total : ret;
+
+		memcpy((u8 *)dest + total, decBuf + pageOff, toCopy);
+		total += toCopy;
+		file->pos += toCopy;
+	}
+
+	return (ssize_t)total;
+}
+
+static ssize_t sffsSeek(struct filesystem *fs, int fd, ssize_t off) {
+	(void)fs;
+	(void)off;
+	VALIDATE_FD(-EBADF);
+
+	if (off < 0 || (u64)off > (u64)files[fd].size)
+		return (ssize_t)-1;
+
+	files[fd].pos = (u32)off;
+	return off;
+}
+
 static ssize_t sffsGetSize(struct filesystem *fs, int fd) {
 	(void)fs;
 	VALIDATE_FD(-EBADF);
 
 	return (ssize_t)files[fd].size;
 }
-
-
 
 struct filesystem FS_SFFS = {
 	.name = "SFFS",
@@ -249,7 +346,7 @@ struct filesystem FS_SFFS = {
 	.unmount = sffsUnmount,
 	.open = sffsOpen,
 	.close = sffsClose,
-	.read = NULL,
-	.seek = NULL,
+	.read = sffsRead,
+	.seek = sffsSeek,
 	.getSize = sffsGetSize,
 };
