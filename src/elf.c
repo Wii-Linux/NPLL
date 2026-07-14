96107e811434ee66d6eed9d582d6f78b1d133fbd
techflashYT <officialTechflashYT@gmail.com>
2026-07-13 19:09:42 -0500

    elf: fixup vmlinux entry

--- src/elf.c
+++ src/elf.c
@@ -252,10 +252,11 @@ static int _elfLoadFile(int fd, const void *dtb, const void *initrd, u32 initrdS
 	void *addr;
 	u32 size;
 	uint i;
-	bool load;
+	bool load, linuxEntryFound = false;
 	int ret;
 	ssize_t res;
 	size_t off;
+	u32 linuxEntry = 0;
 
 	/* read in the ehdr */
 	res = FS_Seek(fd, 0);
@@ -301,6 +302,18 @@ static int _elfLoadFile(int fd, const void *dtb, const void *initrd, u32 initrdS
 		else if (!load)
 			continue;
 
+		if (dtb && ehdr.e_entry >= phdr.p_vaddr &&
+		    ehdr.e_entry - phdr.p_vaddr < phdr.p_memsz) {
+			u32 entryOff = ehdr.e_entry - phdr.p_vaddr;
+
+			if (phdr.p_paddr + entryOff < phdr.p_paddr) {
+				log_puts("Linux physical entry address overflows");
+				return ELF_ERR_INVALID_EXEC;
+			}
+			linuxEntry = phdr.p_paddr + entryOff;
+			linuxEntryFound = true;
+		}
+
 		res = FS_Seek(fd, (ssize_t)off);
 		if (res != (ssize_t)off) {
 			log_printf("FS_Seek for segment %d returned: %d\r\n", i, res);
@@ -330,6 +343,10 @@ static int _elfLoadFile(int fd, const void *dtb, const void *initrd, u32 initrdS
 
 	/* close the file */
 	FS_Close(fd);
+	if (dtb && !linuxEntryFound) {
+		log_printf("Linux entry %08x is not in a PT_LOAD segment\r\n", ehdr.e_entry);
+		return ELF_ERR_INVALID_EXEC;
+	}
 
 	/* get ready to jump ship (shut down subsystems, ack, mask, and disable IRQs, etc) */
 	H_PrepareForExecEntry();
@@ -342,10 +359,11 @@ static int _elfLoadFile(int fd, const void *dtb, const void *initrd, u32 initrdS
 
 	/* lets do this thing */
 	if (dtb) {
-		u32 entry = (u32)(uintptr_t)virtToPhys((uintptr_t)ehdr.e_entry);
+		u32 entry = linuxEntry;
 		u32 dtbPhys = (u32)(uintptr_t)virtToPhys(dtb);
 
 		ELF_InstallLinuxData(entry, initrd, initrdSize, cmdline, cmdlineFlags);
+		log_printf("ELF: entering Linux at %08x with DTB %08x\r\n", entry, dtbPhys);
 		ELF_DoEntry(dtbPhys, entry, 0, (void *)(uintptr_t)entry, true);
 	} else {
 		u32 entry = (u32)(uintptr_t)virtToPhys((uintptr_t)ehdr.e_entry);
